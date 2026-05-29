8805ecc0a7345b028eab1825844d2c795ab19d2a
emilio <emilio.trabalho555@gmail.com>
2026-05-29 10:36:02 -0300

    peças criada por de baixo dos planos

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,16 +1,39 @@
-DOM ={
-    player1: document.getElementById("player1"),
-    player2: document.getElementById("player2"),
-    player3: document.getElementById("player3"),
-    player4: document.getElementById("player4"),
-    table: document.getElementById("table")
+DOM = {
+  player1: document.getElementById("player1"),
+  player2: document.getElementById("player2"),
+  player3: document.getElementById("player3"),
+  player4: document.getElementById("player4"),
+  table: document.getElementById("table"),
+};
+
+function createPartsStyle() {
+  for (let i = 1; i <= 4; i++) {
+    for (let e = 1; e <= 7; e++) {
+      let parts = document.createElement("span");
+      DOM[`player${i}`].appendChild(parts);
+      parts.className = `player${i}-span`;
+      parts.id = `number${e}`;
+    }
+  }
 }
 
-for(let i = 1; i <= 4; i++){
-for(let e = 1; e <=7; e++){
-    let parts = document.createElement("spam");
-    DOM[`player${i}`].appendChild(parts);
-    parts.className = `player${i}-spam`;
-    parts.id = `number${e}`;
+let partsPlayer = [];
+function createParts() {
+    for(let i = 0; i <= 6; i++){
+        for(let e = i; e <= 6; e++){
+            partsPlayer.push({
+                sideA: i,
+                sideB: e,
+                type:  (i === e) ? "dupla" : "comum"
+            });
+        }
     }
+
 }
+
+
+
+createParts();
+createPartsStyle();
+
+console.log(partsPlayer)
